793d8ee84ef0f0ce6c98e8425dccfba6b0d037df
Vasil <vasil.todorov@student.fontys.nl>
2022-10-15 14:14:20 +0200

    add enemy class

--- script.js
+++ script.js
@@ -2,4 +2,29 @@
 const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext("2d");
 CANVAS_WIDTH = canvas.width = 500;
-CANVAS_HEIGHT = canvas.height = 500;
+CANVAS_HEIGHT = canvas.height = 1000;
+
+class Enemy {
+    constructor(){
+        this.x = 10;
+        this.y = 50;
+        this.width = 100;
+        this.height = 100;
+    }
+    update(){
+        this.x++;
+        this.y++;
+    }
+    draw(){
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+}
+const enemy1 = new Enemy();
+
+function animate() {
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    enemy1.update();
+    enemy1.draw();
+    requestAnimationFrame(animate);
+}
+animate()
