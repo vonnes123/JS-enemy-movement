547301176521150f1a7b6180c044e4dc8168a6a0
Vasil <vasil.todorov@student.fontys.nl>
2022-10-16 11:24:04 +0200

    wave movement complete for enemy 2

--- script.js
+++ script.js
@@ -11,20 +11,25 @@ let gameFrame = 0;
 class Enemy {
     constructor(){
         this.image = new Image();
-        this.image.src = "enemy1.png";
-        //this.speed = Math.random() * 4 - 2;
-        this.spriteWidth = 293;
-        this.spriteHeight = 155;
+        this.image.src = "enemy2.png";
+        this.speed = Math.random() * 4 + 1;
+        this.spriteWidth = 266;
+        this.spriteHeight = 188;
         this.width = this.spriteWidth / 2.5;
         this.height = this.spriteHeight / 2.5;
         this.x = Math.random() * (canvas.width - this.width);
         this.y = Math.random() * (canvas.height - this.height);
         this.frame = 1;
         this.flapSpeed = Math.floor(Math.random() * 3 + 1);
+        this.angle = 0;
+        this.angleSpeed = Math.random() * 0.2;
+        this.curve = Math.random() * 7;
     }
     update(){
-        this.x += Math.random() * 5 -2.5;
-        this.y += Math.random() * 5 - 2.5;
+        this.x -= this.speed;
+        this.y += this.curve * Math.sin(this.angle);
+        this.angle += this.angleSpeed;
+        if(this.x + this.width < 0) this.x = canvas.width;
         //animate sprite
         if(gameFrame % this.flapSpeed === 0) {
         this.frame > 4 ? this.frame = 0 : this.frame++;
