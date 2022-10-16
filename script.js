f9cb20ebfab918a98a3bcc10694e7591f539db65
Vasil <vasil.todorov@student.fontys.nl>
2022-10-16 11:42:47 +0200

    add enemey movement 3

--- script.js
+++ script.js
@@ -11,10 +11,10 @@ let gameFrame = 0;
 class Enemy {
     constructor(){
         this.image = new Image();
-        this.image.src = "enemy2.png";
+        this.image.src = "enemy3.png";
         this.speed = Math.random() * 4 + 1;
-        this.spriteWidth = 266;
-        this.spriteHeight = 188;
+        this.spriteWidth = 218;
+        this.spriteHeight = 177;
         this.width = this.spriteWidth / 2.5;
         this.height = this.spriteHeight / 2.5;
         this.x = Math.random() * (canvas.width - this.width);
@@ -22,17 +22,18 @@ class Enemy {
         this.frame = 1;
         this.flapSpeed = Math.floor(Math.random() * 3 + 1);
         this.angle = 0;
-        this.angleSpeed = Math.random() * 0.2;
-        this.curve = Math.random() * 7;
+        this.angleSpeed = Math.random() * 1.5 + 0.5;
+        //this.curve = Math.random() * 200 + 50;
     }
     update(){
-        this.x -= this.speed;
-        this.y += this.curve * Math.sin(this.angle);
+        this.x = canvas.width/2 * Math.sin(this.angle * Math.PI/90) + (canvas.width/2 - this.width/2);
+        this.y = canvas.height/2 * Math.cos(this.angle * Math.PI/360) +
+        (canvas.height / 2 - this.height / 2);
         this.angle += this.angleSpeed;
         if(this.x + this.width < 0) this.x = canvas.width;
         //animate sprite
         if(gameFrame % this.flapSpeed === 0) {
-        this.frame > 4 ? this.frame = 0 : this.frame++;
+            this.frame > 4 ? this.frame = 0 : this.frame++;
         }
     }
     draw(){
